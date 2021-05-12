17d21f1d183cec4a7b4b4494373e7778b586186f
M Schachnow <mschachnow@naalehcollege.edu>
2021-05-12 19:48:11 -0400

    Homework 43

---
+++ hw44.py
@@ -0,0 +1,38 @@
+import random
+# 1.
+name = 'Dena Klein'
+print(name)
+address = '5 Green Dr'
+print(address)
+names = ['Shifra', 'Motti', 'Rivky', 'Sori', 'Brochi']
+print(names)
+# 2.
+print(f'Name:  {name} Address:  {address} Names:  {names}')
+# 3.
+print(name[::3])
+
+# 4.
+print(names[-2][1:-1])
+# 5.
+for i in range(1, 11):
+    for j in range(1,11):
+        print(i*j)
+# 6.
+number = random.randint(1, 10)
+print(number)
+
+guess =0
+while guess != number:
+    try:
+        guess= int(input('Guess a number between 1 and 10: '))        
+        if number == guess:
+            print('You Won!')
+        elif  number > guess:
+            print('you guessed to low.')
+        else:
+            print('You guessed to high.')
+           
+    except ValueError as e:
+        print('Non Valid')
+        
+
