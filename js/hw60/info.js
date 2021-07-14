01003a939a5865ba4517723be5048e6db78e25af
M Schachnow <mschachnow@naalehcollege.edu>
2021-07-13 21:58:40 -0400

    Homework 60

---
+++ js/hw60/info.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var name='Joe Shmoe';
+var email='joesh@gmail';
+
+
+
---
+++ js/hw60/print.js
@@ -0,0 +1,9 @@
+'use strict';
+/*globals name, email*/
+
+console.log(name,email);
+
+var age = prompt('Enter Age: ')
+console.log('Age = '+ age);
+
+
