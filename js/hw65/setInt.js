74870f28232e1414e4d63ad85a4903feaf2d6d80
M Schachnow <mschachnow@naalehcollege.edu>
2021-08-24 22:15:34 -0400

    Homework 65

---
+++ js/hw65/setInt.js
@@ -0,0 +1,30 @@
+let i= 0;
+(function () {
+    'use strict';
+    
+    const lorem = document.getElementById('lorem');
+    const colors = ['violet', 'orange', 'yellow', 'green', 'lightBlue'];
+    const textColors = ['black', 'white', 'purple', 'pink', 'grey'];
+
+    function changeColor() {
+         lorem.style.backgroundColor = colors[i];
+         lorem.style.color = textColors[i];
+         i = i === colors.length ? 0 : i+1;
+         
+    }
+
+    let theIntervalId;
+    const startButton = document.getElementById('startButton');
+    startButton.addEventListener('click', () => {
+        if(!theIntervalId) {
+            theIntervalId = setInterval(changeColor, 2000);
+            startButton.innerText = 'Stop';
+        } else {
+            clearInterval(theIntervalId);
+            theIntervalId = undefined;
+            startButton.innerText = 'Start';
+        }
+    });
+    
+    
+}());
