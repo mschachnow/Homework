78c2d80dc2e1374067aa5fb16e7bf7363f0f9fdb
M Schachnow <mschachnow@naalehcollege.edu>
2021-10-05 22:56:25 -0400

    Homework 70

---
+++ js/hw69/clock.js
@@ -0,0 +1,32 @@
+
+window.pcs = window.pcs || {};
+window.pcs.clock = (function () {
+    'use strict';
+    function createClock() {
+
+        let clock = document.createElement('div');
+        document.body.appendChild(clock);
+
+        function currentTime() {
+
+            let date = new Date();
+            let hh = date.getHours();
+            let mm = date.getMinutes();
+            let ss = date.getSeconds();
+            let session = "AM";
+            if (hh === 0) {
+                hh = 12;
+            }
+            if (hh > 12) {
+                hh = hh - 12;
+                session = "PM";
+            }
+
+            clock.innerText = (hh + ":" + mm + ":" + ss + " " + session);
+
+        }
+        setInterval(currentTime, 1000);
+    }
+    return { createClock };
+
+}());
---
+++ js/hw69/jquery.js
@@ -0,0 +1,65 @@
+window.pcs = (function (id) {
+    'use strict';
+
+    let i = 0;
+    const colors = ['violet', 'orange', 'yellow', 'green', 'lightBlue'];
+
+
+    function changeColor() {
+        theElem.style.color = colors[i];
+        i = i === colors.length ? 0 : i + 1;
+    }
+
+    function get(id) {
+        return document.getElementById(id);
+    }
+
+    function setCss(elem, prop, value) {
+        elem.style[prop] = value;
+    }
+
+    function getCss(elem, prop) {
+        return getComputedStyle(elem)[prop];
+    }
+
+    /*function css(elem, prop, value) {
+      if (!value) {
+        return getComputedStyle(elem)[prop];
+      }
+      elem.style[prop] = value;
+    }*/
+
+
+
+    const theElem = get(id);
+
+    return {
+        /*setCss: (prop, value) => setCss(theElem, prop, value),
+        getCss: (prop) => getCss(theElem, prop)*/
+        css: function (prop, value) {
+            if (arguments.length === 1) {
+                return getCss(theElem, prop);
+            }
+            setCss(theElem, prop, value);
+            return this;
+        },
+        click: function (callback) {
+            theElem.addEventListener('click', callback);
+            return this;
+        },
+        hide: function () {
+            setCss(theElem, 'display', 'none');
+            return this;
+        },
+        show: function () {
+            setCss(theElem, 'display', 'block');
+            return this;
+        },
+        changeColors: function (myInterval, duration) {
+            let interval = setInterval(changeColor, myInterval);
+            setTimeout(() => clearInterval(interval), duration);
+            return this;
+        }
+
+    };
+});
