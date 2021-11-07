a43e7a2d45e147d200f42ba26906299784596716
M Schachnow <mschachnow@naalehcollege.edu>
2021-11-07 16:24:56 -0500

    update Homework 77

--- js/hw77/potatoHead.js
+++ js/hw77/potatoHead.js
@@ -4,9 +4,11 @@
     let bodypart = null;
     let offset;
     let zIndex = 0;
+    const boing = $('#boing').find('audio')[0];
 
     $(document)
         .on('mousedown', '.part', e => {
+
             console.log('mousedown');
             bodypart = $(e.target);
             offset = { x: e.offsetX, y: e.offsetY };
@@ -20,11 +22,27 @@
                 bodypart.css('z-index', zIndex);
             }
         })
+
         .mouseup(() => {
+
             if (bodypart) {
                 console.log('mouseup');
+
                 bodypart = null;
+                boing.play();
+                let image = bodypart.getItem();
+                //console.log(bodypart.getPosition());
+                localStorage.setItem('bodypart', 'image');
+                localStorage.setItem('bodypart', bodypart.getPosition());
+
             }
         });
+    const savedParts = localStorage.getItem('bodypart');
+    console.log(savedParts);
+
+    /*bodypart.click(function () {
+        $('#boing').play();
+        console.log(print);
+    });*/
 
 }());
