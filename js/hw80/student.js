705e7c2680162d95483b8f600d92bff4a3844f55
M Schachnow <mschachnow@naalehcollege.edu>
2021-11-16 22:50:00 -0500

    Homework80

---
+++ js/hw80/student.js
@@ -0,0 +1,43 @@
+
+'use strict';
+
+class Student {
+    constructor(firstName, lastName, age, grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.grade = grade;
+    }
+    print() {
+        console.log(`${this.firstName} ${this.lastName} ${this.age} ${this.grade}`);
+    }
+}
+const s1 = new Student('Nechama', 'Stein', '13', '9th');
+const s2 = new Student('Chaya', 'Kohn', '17', '12th');
+const s3 = new Student('Gitty', 'Schwartz', '15', '11th');
+
+const studentArray = [s1, s2, s3];
+
+function printStudents(...students) {
+    students.forEach(eachStudent => {
+        console.log(`${eachStudent.firstName} ${eachStudent.lastName} ${eachStudent.age} ${eachStudent.grade}`);
+    });
+}
+printStudents(...studentArray);
+//let [one, two, three] = studentArray;
+
+function copy(theStudent) {
+    const { firstName, lastName, ...rest } = theStudent;
+    /*const newStudent = {
+        firstName: lastName,
+        lastName: firstName,
+        ...rest
+    };*/
+    //return Object.assign(new Student(), newStudent);
+    return Object.assign(new Student(lastName, firstName), rest);
+}
+const copyOf = copy(s2);
+console.log(copyOf);
+const copy2 = copy(s3);
+console.log(copy2);
+
