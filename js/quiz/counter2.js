5e62d042ce9b958c26ed0fdb5b406264cc03f8d3
mschachnow <84161935+mschachnow@users.noreply.github.com>
2021-12-09 09:20:06 -0500

    Revert "comments on quiz"

--- js/quiz/counter2.js
+++ js/quiz/counter2.js
@@ -5,23 +5,21 @@ window.myApp.counters = (function (myModule) {
     myModule.inc = (function incrementCounter() {
         this.counter++;
     });
-
-    // SL - so you call create, get back an object with counter in it, and then call inc passing that as this?
-    // interesting approach...
+    
     myModule.create= (function createCounter(name) {
         myModule.countersCreated++;
         return{
-            name,
+            name, 
             counter: 0
         };
     });
     //counters created and incremented from this module
     //myModule.createCount = () => createCounter();
     //myModule.createCounter = (name) => (name, counter: 0, counterCreate: counterCreated+1);
-
+    
     /*const x = myModule.create('x');
     const y = myModule.create('y');
-
+    
     //incrementCounter.call(x);
     //incrementCounter.call(y);
     myModule.inc.call(x);
@@ -33,6 +31,4 @@ window.myApp.counters = (function (myModule) {
     return myModule;
 
 }(window.myApp.counters || {}));
-
-// SL - what you wrote shows good understanding of objects and how to use them - but not really what I was expecting...
 
