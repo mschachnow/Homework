772ad98c4a9f89eace18437854815d5b845b6b10
M Schachnow <mschachnow@naalehcollege.edu>
2022-01-11 22:35:59 -0500

    hw91

--- React/hw91/src/blog.js
+++ React/hw91/src/blog.js
@@ -2,11 +2,11 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => console.log(users))
 
-fetch('https://jsonplaceholder.typicode.com/userId=2')
+fetch('https://jsonplaceholder.typicode.com/posts?userId=2')
     .then(response => response.json())
     .then(users => console.log(users))
 
-fetch('https://jsonplaceholder.typicode.com/postId=5')
+fetch('https://jsonplaceholder.typicode.com/comments?postId=5')
     .then(response => response.json())
     .then(users => console.log(users))
 
