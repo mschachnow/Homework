57a709940f82e25b5c5adc67a8e01cbf86704160
M Schachnow <mschachnow@naalehcollege.edu>
2022-01-17 22:47:12 -0500

    Homework 93

--- React/hw91/src/blog.js
+++ React/hw91/src/blog.js
@@ -1,6 +1,14 @@
+
+
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => console.log(users))
+    .then(users => {
+        console.log(users)
+        users.forEach(user => {
+            $(`<li>${user.name, user.email, user.postId}</li>`).appendTo('#user');
+        });
+    })
 
 fetch('https://jsonplaceholder.typicode.com/posts?userId=2')
     .then(response => response.json())
---
+++ React/hw93/recipes/src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import React, { Component, useState, useEffect } from 'react';
+import RecipeDetails from './RecipeDetails';
+import ListComponent from './ListComponent';
+import ClickCounter from './ClickCounter';
+import RecipeList from './RecipeList';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import Header from './Header';
+
+export default function App() {
+  const [recipeData, setRecipeData] = useState({ recipes: [''] })
+
+  const [recipes, selectedRecipe] = recipeData;
+
+  const selectRecipe = (index) => {
+    console.log('selectRecipe', index);
+    //here goes await fetch recipes
+    setRecipeData({
+      ...recipeData
+    });
+  }
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('recipes.json');
+      const recipes = await response.json();
+      recipes.map(async recipe => {
+        const r = await fetch(`${recipe.id}`);
+        const theRecipe = await r.json();
+        console.log(theRecipe);
+      });
+
+    })();
+    return () => {
+      //reset id
+    }
+  }, []);
+
+  return (
+    <div className='container-fluid'>
+      <div className='text-center'>
+        <Header />
+        <Routes>
+          <Route index element={<RecipeList recipes={recipes} />} />
+
+          <Route path='/recipe/:id' element={<RecipeDetails recipes={recipes} />} />
+
+          {/*<Route path='*' element={<h5 style={{ color: 'red' }}>This is not the page you are looking for - 404</h5>} />*/}
+
+          <Route path='*' element={<Navigate to='/' replace="true" />} />
+        </Routes>
+        <Outlet />
+        <hr />
+        <ClickCounter />
+      </div>
+    </div>
+  );
+}
+
+
+
